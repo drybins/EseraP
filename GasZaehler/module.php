e9b0f29ad268f0c49b06a5bb2570d40b5b959701
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-12-24 15:00:00 +0100

    Update module.php

--- GasZaehler/module.php
+++ GasZaehler/module.php
@@ -19,10 +19,7 @@ class EseraGaszaehler extends IPSModule
 		$this->RegisterVariableFloat("VerbrauchTagm", "Verbrauch am Tag in m³", "~Gas", 4);
 		$this->RegisterVariableFloat("VerbrauchTagkwh", "Verbrauch am Tag in kwh", "Kirsch.kWh", 5);
 		$this->RegisterVariableFloat("VerbrauchVortagm", "Verbrauch Vortag in m³", "~Gas", 6);
-		$ArchiveHandlerID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
-	    //	$AID =  $this->GetIDForIdent("Archive");
-		echo "ArchiveID = $ArchiveHandlerID \r\n";
-	    	//AC_SetLoggingStatus(39147 /*[Archive]*/, 53716 /*[TestVariable]*/, true);
+		
 		$this->RegisterVariableFloat("VerbrauchVortagkwh", "Verbrauch Vortag in kWh", "Kirsch.kWh", 7);
 		$this->RegisterVariableFloat("VerbrauchVortagEuro", "Verbrauch Vortag in €", "~Euro", 8);
 		
@@ -41,6 +38,9 @@ class EseraGaszaehler extends IPSModule
 	{
         //Never delete this line!
         parent::ApplyChanges();
+		$ArchiveHandlerID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
+		echo "ArchiveID = $ArchiveHandlerID \r\n";
+	    	//AC_SetLoggingStatus(39147 /*[Archive]*/, 53716 /*[TestVariable]*/, true);
         $this->SetTimerInterval("Refresh", 180 * 1000);
         $this->SetDailyTimerInterval();
         //$this->SetMonthlyTimerInterval();
