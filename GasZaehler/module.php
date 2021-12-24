addd25a239b3bbb19fda11f0d84e80e1b458b20f
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-12-24 14:16:22 +0100

    Update module.php

--- GasZaehler/module.php
+++ GasZaehler/module.php
@@ -19,6 +19,9 @@ class EseraGaszaehler extends IPSModule
 		$this->RegisterVariableFloat("VerbrauchTagm", "Verbrauch am Tag in m³", "~Gas", 4);
 		$this->RegisterVariableFloat("VerbrauchTagkwh", "Verbrauch am Tag in kwh", "Kirsch.kWh", 5);
 		$this->RegisterVariableFloat("VerbrauchVortagm", "Verbrauch Vortag in m³", "~Gas", 6);
+	    	$AID = GetIDForIdent("Archive");
+		echo "ArchiveID = $AID \r\n";
+	    	//AC_SetLoggingStatus(39147 /*[Archive]*/, 53716 /*[TestVariable]*/, true);
 		$this->RegisterVariableFloat("VerbrauchVortagkwh", "Verbrauch Vortag in kWh", "Kirsch.kWh", 7);
 		$this->RegisterVariableFloat("VerbrauchVortagEuro", "Verbrauch Vortag in €", "~Euro", 8);
 		
