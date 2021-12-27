de6ebf49c3a010611f7a04f6bb5e544367a2a6d7
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-12-27 13:47:47 +0100

    Update module.php

--- GasZaehler/module.php
+++ GasZaehler/module.php
@@ -125,11 +125,11 @@ class EseraGaszaehler extends IPSModule
 		SetValue($this->GetIDForIdent("Verbrauch"), $delta_qm);
 		
 		// Only for debugging
-        $this->DebugMessage("Counter", "CounterOld: " . $CounterOld);
-        $this->DebugMessage("Counter", "CounterNew: " . $CounterNew);
-        $this->DebugMessage("Counter", "Delta: " . $delta);
-        $this->DebugMessage("Counter", "Factor: " . $Factor);
-        $this->DebugMessage("Counter", "Delta kWh: " . $delta_qm);
+        $this->DebugMessage("Gas Zähler", "CounterOld: " . $CounterOld);
+        $this->DebugMessage("Gas Zähler", "CounterNew: " . $CounterNew);
+        $this->DebugMessage("Gas Zähler", "Delta: " . $delta);
+        $this->DebugMessage("Gas Zähler", "Factor: " . $Factor);
+        $this->DebugMessage("Gas Zähler", "Delta kWh: " . $delta_qm);
 		
 		//Counter Tag
 		$CounterTag = GetValue($this->GetIDForIdent("TagCounter")) + $delta;
